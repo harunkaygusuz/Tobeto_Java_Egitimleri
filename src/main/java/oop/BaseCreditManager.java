7a0f9a75d537b236dd430acdb1b02f2cbc0f591d
Harun Kaygusuz <151049377+harunkaygusuz@users.noreply.github.com>
2024-02-09 00:52:10 +0300

    oopvecleancode

---
+++ src/main/java/oop/BaseCreditManager.java
@@ -0,0 +1,11 @@
+package oop;
+
+public abstract class BaseCreditManager implements ICreditManager{
+    @Override
+    public abstract void calculate();
+
+    @Override
+    public void save(){
+        System.out.println("Kaydedildi.");
+    }
+}
